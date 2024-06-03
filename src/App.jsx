3200e8bfd63ac8ab4befe01bdfcc234c36906445
Michal Nithesh <151201789+Nithesh14@users.noreply.github.com>
2024-06-03 18:43:23 +0000

    Learning Git

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Michal Nithesh",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  email: "michalnithesh@example.com",
+  gitHub: "Nithesh14",
+  instagram: "michal.nithesh",
+  linkedIn: "Michal Nithesh",
   medium: "",
-  twitter: "microsoft",
+  twitter: "michalnithesh",
   youTube: "Code",
 };
 
